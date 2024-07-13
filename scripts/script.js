60f6e47b32a4959057e6246b1aed8d1834e8de01
Bailey Lucketta <blucketta@student.cccs.edu>
2024-07-12 18:09:57 -0600

    mobile update 1

--- scripts/script.js
+++ scripts/script.js
@@ -7,7 +7,7 @@ Date: 07/07/2024
 //Hamburger menu function
 function hamburger() {
     var menu = document.getElementById("menu-links");
-    var logo = document.getElementById("ffc-logo");
+    var logo = document.getElementById("l-logo");
 
     if (menu.style.display === "block" && logo.style.display === "none") {
         menu.style.display = "none";
